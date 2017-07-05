680229d667e663134224ce8cd509234660e86d98
isoriaa <isoria@fttn.es>
2017-07-05 18:42:58 +0200

    no message

--- Ejercicio01-Ivan/Ejercicio01-Ivan.ino
+++ Ejercicio01-Ivan/Ejercicio01-Ivan.ino
@@ -5,12 +5,13 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(PIN_LED,OUTPUT);
+  pinMode(PIN_LED, OUTPUT);
+  digitalWrite(PIN_LED, 0);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(PIN_LED,HIGH);
+  digitalWrite(PIN_LED, 1);
   delay(1000);
-  digitalWrite(PIN_LED,LOW);
+  digitalWrite(PIN_LED, 0);
 }
