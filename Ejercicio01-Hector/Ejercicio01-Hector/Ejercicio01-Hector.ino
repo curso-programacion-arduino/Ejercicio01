f7d957de8255c9c709cb1c1479ff171edc1d5a68
Héctor <erhector12@hotmail.com>
2017-07-05 19:01:31 +0200

    modificación

--- Ejercicio01-Hector/Ejercicio01-Hector/Ejercicio01-Hector.ino
+++ Ejercicio01-Hector/Ejercicio01-Hector/Ejercicio01-Hector.ino
@@ -1,13 +1,27 @@
 void setup() {
   // put your setup code here, to run once:
   pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(4, HIGH);   
-  delay(1000);                       
-  digitalWrite(4, LOW);    
-  delay(1000);
+
+  for (int i = 4; i <= 7; i++) {
+    digitalWrite(i, HIGH);
+    delay(1000);
+    digitalWrite(i, LOW);
+    delay(1000);
+  }
+
+for (int i = 7; i >= 4; i--) {
+    digitalWrite(i, HIGH);
+    delay(1000);
+    digitalWrite(i, LOW);
+    delay(1000);
+  }
+  
 }
